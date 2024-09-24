b7baf056096c3298c153aade649de361f69de2ae
Deep <dp.rahman@gmail.com>
2024-09-24 21:55:42 +0600

    Changes to be committed: 	new file:   .circleci/config.yml 	new file:   .phpcs.xml.dist 	new file:   bin/install-wp-tests.sh 	deleted:    composer.json 	deleted:    composer.lock 	renamed:    src/class-wpss-directives.php -> includes/class-wpss-directives.php 	renamed:    src/class-wpss-file-permission-manager.php -> includes/class-wpss-file-permission-manager.php 	renamed:    src/class-wpss-securing-setup.php -> includes/class-wpss-securing-setup.php 	renamed:    src/pattern_modifier.php -> includes/pattern_modifier.php 	deleted:    phpunit.xml 	new file:   phpunit.xml.dist 	deleted:    tests/WPSS_File_Permission_Manager_Test.php 	modified:   tests/bootstrap.php 	new file:   tests/test-sample.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,23 +1,38 @@
 <?php
 /**
- * PHPUnit Bootstrap File
+ * PHPUnit bootstrap file.
  *
- * This file is used to set up the testing environment for your WordPress plugin or theme.
- * It includes the WordPress `wp-load.php` file, which loads the WordPress environment.
- *
- * Make sure to update the path to the `wp-load.php` file to match your local setup.
+ * @package Wp_Securing_Setup
  */
-// Path to the WordPress file autoloader
-require_once __DIR__ . "/../wp-autoloader.php";
-// Path to the WordPress directory
-$wp_root = '/home/deep/Websites/woocommerce.lo/wp';
 
-// Load the WordPress environment
-require_once $wp_root . '/wp-load.php';
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
 
-// Set up the testing environment
-global $wp, $wp_query, $current_site, $current_blog;
-$wp->init();
-$wp->register_globals();
-//$current_site = $wp->get_current_site();
-//$current_blog = $wp->get_current_blog_id();
+if ( ! $_tests_dir ) {
+	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
+}
+
+// Forward custom PHPUnit Polyfills configuration to PHPUnit bootstrap file.
+$_phpunit_polyfills_path = getenv( 'WP_TESTS_PHPUNIT_POLYFILLS_PATH' );
+if ( false !== $_phpunit_polyfills_path ) {
+	define( 'WP_TESTS_PHPUNIT_POLYFILLS_PATH', $_phpunit_polyfills_path );
+}
+
+if ( ! file_exists( "{$_tests_dir}/includes/functions.php" ) ) {
+	echo "Could not find {$_tests_dir}/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+	exit( 1 );
+}
+
+// Give access to tests_add_filter() function.
+require_once "{$_tests_dir}/includes/functions.php";
+
+/**
+ * Manually load the plugin being tested.
+ */
+function _manually_load_plugin() {
+	require dirname( dirname( __FILE__ ) ) . '/wp-securing-setup.php';
+}
+
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
+// Start up the WP testing environment.
+require "{$_tests_dir}/includes/bootstrap.php";
