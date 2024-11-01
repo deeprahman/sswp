c42b0ef2619b7dfc291b295c639a593a20ec4ac1
Deep Rahman <dp.rahman@gmail.com>
2024-11-01 14:27:30 +0600

    modified:   examples/file-permissions.php
    Example updated

--- examples/file-permissions.php
+++ examples/file-permissions.php
@@ -10,24 +10,32 @@ require_once( ABSPATH .  "wp-content/plugins/wp-securing-setup/includes/interfac
 require_once(ABSPATH . "wp-content/plugins/wp-securing-setup/includes/class-wpss-file-permission-manager.php");
 //
 
-$paths = ["wp-config.php", "wp-login.php", "wp-content", "wp-content/uploads", "wp-content/plugins", "wp-content/themes"];
+$paths = [ "wp-config-sample.php","wp-config.php", "wp-login.php", "wp-content", "wp-content/uploads", "wp-content/plugins", "wp-content/themes"];
 //
 
 
 try{
-$pm = new WPSS_File_Permission_Manager($paths);
+    //$pm = new WPSS_File_Permission_Manager($paths);
+    $file = ABSPATH . "wp-config-sample.php";
+    echo "old param ". decoct(fileperms($file)) . " \n";
+    $perms = '0444';
+    echo "Perms to be set ".$perms."\n";
+    $c = chmod($file,octdec($perms));
+    clearstatcache(); 
+    echo "New perms" . decoct(fileperms($file)) . "\n";
 
-if (!function_exists('WP_Filesystem')) {
-    require_once(ABSPATH . 'wp-admin/includes/file.php');
-    WP_Filesystem();
-}
-$wp_filesystem->chmod(ABSPATH . 'wp-config.php', 777);
-$perms = $wp_filesystem->chmod(ABSPATH . 'wp-config.php');
+    //echo "Permission of the file ". $file. " is ". $pm->get_current_permission($file) . "\n";
+    //$pm->change_file_permission($file, $perms);
 
-echo "The permission of " ."wp-config.php". " is ". $perms  . "\n";
+    //$r = $pm->get_current_permission($file);
 
-$pm->display_results();
+    //echo "After Change:  Permission of the file ". $file. " is ". $r. "\n";
+
+//    echo "After Change: Using - fileperms() -  Permission of the file ". $file. " is ". decoct(fileperms($file)). "\n";
+
+    //$pm->display_results();
+
 }catch(Exception $e){
-    printf($e->getMessage());
+    die($e->getMessage());
 }
 
