1c63c55bb04ceca3e56bb22c33d4c7acb87661e0
Deep Rahman <dp.rahman@gmail.com>
2024-11-18 16:26:32 +0600

    class attributes added

--- admin/templates/page.htm.php
+++ admin/templates/page.htm.php
@@ -7,15 +7,15 @@ global $wpss;
 ?>
 
 
-<h1> <?php _e("WP Securing Setup", $wpss->domain) ?> </h1>
+<h1 id="wpss-page-heading"> <?php _e("WP Securing Setup", $wpss->domain) ?> </h1>
 <hr>
-<h2> <?php _e("File Permission Page", $wpss->domain) ?> </h2>
+<h2  id="wpss-tab-heading"> <?php _e("File Permission Page", $wpss->domain) ?> </h2>
 
 <div id="my-tabs">
   <ul>
     <li><a href="#tab-1"><?php _e("File Permission Page", $wpss->domain) ?></a></li>
     <li><a href="#tab-2"><?php _e(".htacces Config", $wpss->domain) ?></a></li>
-    <li><a href="#tab-3">Form 3</a></li>
+    <li><a href="#tab-3"><?php _e("Site Migration", $wpss->domain) ?></a></li>
   </ul>
   <div id="tab-1">
     <wp-permissions-table></wp-permissions-table>
@@ -25,9 +25,7 @@ global $wpss;
   </div>
   <div id="tab-3">
     <form id="form-3" class="tab-form" disabled>
-      <input type="text" name="product" placeholder="Product">
-      <input type="number" name="quantity" placeholder="Quantity">
-      <button type="submit">Submit Form 3</button>
+        <h3><?php _e("Comming Soon...", $wpss->domain) ?></h3>
     </form>
   </div>
 </div>
