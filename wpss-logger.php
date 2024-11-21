47335b0df4a1c5d7040ab2d45daae60763731fea
Deep Rahman <dp.rahman@gmail.com>
2024-11-21 19:09:22 +0600

    Logger updated

--- wpss-logger.php
+++ wpss-logger.php
@@ -5,19 +5,24 @@
  */
 if (!function_exists('write_log')) {
     function write_log($log, $function =  __FUNCTION__) {
-        if (true === WP_DEBUG) {
+
+        $formatted_log = '[' . date('Y-m-d H:i:s') . '] ' . ' Function: ' . $function . " ";
+        if (is_array($log) || is_object($log)) {
+            $formatted_log .= print_r($log, true);
+        } else {
+            $formatted_log .= $log;
+        }
+
+        $formatted_log .= PHP_EOL;
+
+        if (true === WP_DEBUG && defined(WPSS_ROOT)) {
             //$log_file = WP_CONTENT_DIR . '/wpss.log';
             $log_file = WPSS_ROOT . 'wpss.log';
-            
-            $formatted_log = '[' . date('Y-m-d H:i:s') . '] ' . ' Function: ' . $function . " ";
-            if (is_array($log) || is_object($log)) {
-                $formatted_log .= print_r($log, true);
-            } else {
-                $formatted_log .= $log;
-            }
-            $formatted_log .= PHP_EOL;
-            
+
+
             file_put_contents($log_file, $formatted_log, FILE_APPEND);
+        }else{
+            error_log($formatted_log);
         }
     }
 }
