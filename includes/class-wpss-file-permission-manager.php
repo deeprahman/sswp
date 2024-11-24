a7164fbae9231ae2007ecc43708ef9ffe45775e8
Deep Rahman <dp.rahman@gmail.com>
2024-11-24 18:31:05 +0600

    Implemented conditional owner check

--- includes/class-wpss-file-permission-manager.php
+++ includes/class-wpss-file-permission-manager.php
@@ -199,211 +199,232 @@ class WPSS_File_Permission_Manager {
 		echo str_repeat( '-', $total_width ) . "\n";
 	}
 
-	/**
-	 * Change the file permission to a given permission value.
-	 *
-	 * @param string $path The path to the file or directory.
-	 * @param string $permission The permission to set (e.g., '0644', '0755').
-	 * @return bool True if the permission was changed successfully, false otherwise.
-	 */
-	public function change_file_permission( $path, $permission ) {
-		global $wp_filesystem;
+        public function check_path_validity($path, $enforec_ownership_check = false){
 
-		if ( ! function_exists( 'WP_Filesystem' ) ) {
-			require_once ABSPATH . 'wp-admin/includes/file.php';
-		}
+            global $wp_filesystem;
 
-		WP_Filesystem();
+            if ( ! function_exists( 'WP_Filesystem' ) ) {
+                require_once ABSPATH . 'wp-admin/includes/file.php';
+            }
 
-		if ( ! $this->is_within_wordpress( $path ) ) {
-			return false;
-		}
+            WP_Filesystem();
+            if ( ! $this->is_within_wordpress( $path ) ) {
+                wpss_logger('Info ', 'The path is not within WordPress installation. '. $path, __METHOD__);
+                return false;
+            }
 
-		if ( ! $wp_filesystem->exists( $path ) ) {
-			return false;
-		}
+            if ( ! $wp_filesystem->exists( $path ) ) {
+                wpss_logger('Info ', 'File/Dir does not exists. '. $path, __METHOD__);
+                return false;
+            }
 
-		if ( ( $this->get_ownership_Info( $path )['is_wp_owner'] !== true ) ) {
-			return false;
-		}
+            if ( ( $this->get_ownership_Info( $path )['is_wp_owner'] !== true ) ) {
 
-		return $this->update_permission( $path, $permission );
-	}
+                wpss_logger('Info ', 'Path is not Owned by WordPress Process '. $path, __METHOD__);
+                if($enforec_ownership_check){
+                    return false;
+                }
+            }
+        }
 
-	/**
-	 * Change the directory and file permissions to recommended values.
-	 *
-	 * @param array $paths The array of paths to change permissions for
-	 * @return array Contains paths for which permission could not be changed
-	 */
-	public function change_to_recommended_permissions( array $paths ) {
-		if ( empty( $paths ) ) {
-			return array();
-		}
+        /**
+         * Change the file permission to a given permission value.
+         *
+         * @param string $path The path to the file or directory.
+         * @param string $permission The permission to set (e.g., '0644', '0755').
+         * @return bool True if the permission was changed successfully, false otherwise.
+         */
+        public function change_file_permission( $path, $permission ) {
 
-		$errors = array_filter(
-			$paths,
-			function ( $path ) {
 
-				// Get absolute path
-				$abs_path = ABSPATH . $path;
+            if ( is_wp_error( $this->is_valid_path( $path ) ) ) {
+                return false;
+            }
+            return $this->update_permission( $path, $permission );
+        }
 
-				// Get recommended permission based on whether it's a file or directory
-				$recommended_permission = $this->get_recommended_permission( $abs_path );
+        /**
+         * Change the directory and file permissions to recommended values.
+         *
+         * @param array $paths The array of paths to change permissions for
+         * @return array Contains paths for which permission could not be changed
+         */
+        public function change_to_recommended_permissions( array $paths ) {
+            if ( empty( $paths ) ) {
+                return array();
+            }
 
-				if ( is_wp_error( $recommended_permission ) ) {
-					error_log( 'Code: ' . $recommended_permission->get_error_code() . 'Message: ' . $recommended_permission->get_error_message() . 'Error Data: ' . $recommended_permission->get_error_data() );
-					return true;
-				}
+            $errors = array_filter(
+                $paths,
+                function ( $path ) {
 
-				if ( is_wp_error( $this->is_valid_path( $abs_path ) ) ) {
-					return true;
-				}
+                    // Get absolute path
+                    $abs_path = ABSPATH . $path;
 
-				// Attempt to change the permission
-				$result = $this->update_permission( $abs_path, $recommended_permission );
+                    // Get recommended permission based on whether it's a file or directory
+                    $recommended_permission = $this->get_recommended_permission( $abs_path );
 
-				// Return true if there was an error (to keep this path in the errors array)
-				return is_wp_error( $result ) || $result === false;
-			}
-		);
+                    if ( is_wp_error( $recommended_permission ) ) {
+                        error_log( 'Code: ' . $recommended_permission->get_error_code() . 'Message: ' . $recommended_permission->get_error_message() . 'Error Data: ' . $recommended_permission->get_error_data() );
+                        return true;
+                    }
 
-		// Log any errors that occurred
-		if ( ! empty( $errors ) ) {
-			array_walk(
-				$errors,
-				function ( $error ) {
-					global $wpss;
-					error_log( __( 'Could not change file permission: ' . $error, $wpss->domain ) );
-				}
-			);
-		}
+                    if ( is_wp_error( $this->is_valid_path( $abs_path ) ) ) {
+                        return true;
+                    }
 
-		return array_values( $errors );
-	}
+                    // Attempt to change the permission
+                    $result = $this->update_permission( $abs_path, $recommended_permission );
 
-	/**
-	 * Get the current permission for a given path.
-	 *
-	 * @param string $path The path to check permissions for.
-	 * @return string|null The current permission string or null if the file doesn't exist.
-	 */
-	public function get_current_permission( $path ) {
-		$info = $this->get_file_permission( $path );
-		return $info['exists'] ? $info['permission'] : null;
-	}
+                    // Return true if there was an error (to keep this path in the errors array)
+                    return is_wp_error( $result ) || $result === false;
+                }
+            );
 
-	/**
-	 * Set the recommended permission for a given path type.
-	 *
-	 * @param string $type The type of path ('directory' or 'file').
-	 * @param string $permission The permission to set (e.g., '644', '755').
-	 * @return bool True if the recommended permission was set successfully, false otherwise.
-	 */
-	public function set_recommended_permission( $type, $permission ) {
-		if ( ! in_array( $type, array( 'directory', 'file' ) ) ) {
-			return false;
-		}
+            // Log any errors that occurred
+            if ( ! empty( $errors ) ) {
+                array_walk(
+                    $errors,
+                    function ( $error ) {
+                        global $wpss;
+                        error_log( __( 'Could not change file permission: ' . $error, $wpss->domain ) );
+                    }
+                );
+            }
 
-		if ( ! preg_match( '/^[0-7]{3}$/', $permission ) ) {
-			return false;
-		}
+            return array_values( $errors );
+        }
 
-		$this->recommended_permissions[ $type ] = $permission;
-		return true;
-	}
+        /**
+         * Get the current permission for a given path.
+         *
+         * @param string $path The path to check permissions for.
+         * @return string|null The current permission string or null if the file doesn't exist.
+         */
+        public function get_current_permission( $path ) {
+            $info = $this->get_file_permission( $path );
+            return $info['exists'] ? $info['permission'] : null;
+        }
 
-	/**
-	 * Set the permission for a given path.
-	 *
-	 * @param string $path The path to set permissions for.
-	 * @param string $permission The permission to set (e.g., '644', '755').
-	 * @return bool True if the permission was set successfully, false otherwise.
-	 */
-	public function set_permission( $path, $permission ) {
-		global $wp_filesystem;
-		if ( ! $this->is_within_wordpress( $path ) ) {
-			return false;
-		}
-		WP_Filesystem();
-		if ( ! $wp_filesystem->exists( $path ) ) {
-			return false;
-		}
+        /**
+         * Set the recommended permission for a given path type.
+         *
+         * @param string $type The type of path ('directory' or 'file').
+         * @param string $permission The permission to set (e.g., '644', '755').
+         * @return bool True if the recommended permission was set successfully, false otherwise.
+         */
+        public function set_recommended_permission( $type, $permission ) {
+            if ( ! in_array( $type, array( 'directory', 'file' ) ) ) {
+                return false;
+            }
 
-		return $this->update_permission( $path, $permission );
-	}
+            if ( ! preg_match( '/^[0-7]{3}$/', $permission ) ) {
+                return false;
+            }
 
-	/**
-	 * Updates the file permission
-	 *
-	 * @param string  $path   Absolute path to a file or directory
-	 * @param string  $perms  Permission in octal format (e.g. '0744' , '0444' )
-	 * @param boolean $cc     Clear the state cache after permsiion change
-	 * @return  boolean|WP_Error    On success return true
-	 */
-	private function update_permission( $path, $perms, bool $cc = true ) {
-		$is_changed = $this->wp_filesystem->chmod( $path, octdec( $perms ) );
+            $this->recommended_permissions[ $type ] = $permission;
+            return true;
+        }
 
-		if ( ! $is_changed ) {
-			write_log( 'Function: ' . __METHOD__ . ' Message: ' . 'File Permission Could Not be changed for the path: ' . $path, __METHOD__ );
-			return new WP_Error( '500', 'File Permission Could Not be changed' );
-		}
-		if ( $cc ) {
-			clearstatcache();
-		}
-		return $is_changed;
-	}
+        /**
+         * Set the permission for a given path.
+         *
+         * @param string $path The path to set permissions for.
+         * @param string $permission The permission to set (e.g., '644', '755').
+         * @return bool True if the permission was set successfully, false otherwise.
+         */
+        public function set_permission( $path, $permission ) {
+            global $wp_filesystem;
+            if ( ! $this->is_within_wordpress( $path ) ) {
+                return false;
+            }
+            WP_Filesystem();
+            if ( ! $wp_filesystem->exists( $path ) ) {
+                return false;
+            }
 
-	/**
-	 * File or Directory is Owned by WordPress
-	 *
-	 * @param   $path   Absolute path to the directory or file
-	 * @return  boolean|WP_Error
-	 */
-	public function is_wp_owner( $path ) {
-		$check = $this->check_ownership_permissions( $path );
+            return $this->update_permission( $path, $permission );
+        }
 
-		if ( is_wp_error( $check ) ) {  // TODO: Handle Error
-			write_log( 'Code' . $check->get_error_code() . ' Message: ' . $check->get_error_message(), __METHOD__ );
-			return false;
-		}
+        /**
+         * Updates the file permission
+         *
+         * @param string  $path   Absolute path to a file or directory
+         * @param string  $perms  Permission in octal format (e.g. '0744' , '0444' )
+         * @param boolean $cc     Clear the state cache after permsiion change
+         * @return  boolean|WP_Error    On success return true
+         */
+        private function update_permission( $path, $perms, bool $cc = true ) {
+            $is_changed = $this->wp_filesystem->chmod( $path, octdec( $perms ) );
 
-		// Access detailed information
-		if ( ! $check['ownership']['is_wp_owner'] ) {
-			// Handle incorrect ownership
-			write_log( 'File not owned by WordPress user, File Name: ' . $path, __METHOD__ );
-		}
+            if ( ! $is_changed ) {
+                wpss_logger( 'Info', 'Permission changed for path: ' . $path . ' to ' . $perms, __FUNCTION__ );
+                return new WP_Error( '500', 'File Permission Could Not be changed' );
+            }
+            if ( $cc ) {
+                clearstatcache();
+                wpss_logger( 'Info', 'Cache cleared after permission changed for the path ' . $path, __METHOD__ );
+            }
+            return $is_changed;
+        }
 
-		if ( ! empty( $check['security']['warnings'] ) ) {
-			// Handle security warnings
-			foreach ( $check['security']['warnings'] as $warning ) {
-				write_log( 'Security warning: ' . $warning, __METHOD__ );
-			}
-		}
+        /**
+         * File or Directory is Owned by WordPress
+         *
+         * @param string $path   Absolute path to the directory or file
+         * @return  boolean|WP_Error
+         */
+        public function is_wp_owner( $path ) {
+            $check = $this->check_ownership_permissions( $path );
 
-		return $check;
-	}
+            if ( is_wp_error( $check ) ) {  // TODO: Handle Error
+                write_log( 'Code' . $check->get_error_code() . ' Message: ' . $check->get_error_message(), __METHOD__ );
+                return false;
+            }
 
-	/**
-	 * Check if the given path is valid further processing
-	 *
-	 * @package $path   Absolute path to the file or directory
-	 * @return  boolean|WP_Error    true on success
-	 */
-	private function is_valid_path( string $path ) {
-		global $wpss;
-		if ( ! $this->is_within_wordpress( $path ) ) {
-			return new WP_Error(
-				'invalid_path',
-				__( 'Path is not within WordPress installation', $wpss->domain ),
-				$path
-			);
-		}
-		$is_owned = $this->is_wp_owner( $path );
-		if ( is_wp_error( $is_owned ) ) {
-			write_log( 'Code: ' . $is_owned->get_error_code() . ' Message: ' . $is_owned->get_error_message() . ' Data: ' . $is_owned->get_error_data(), __FUNCTION__ );
-			return false;
-		}
-		return true;
-	}//end is_valid_path()
+            // Access detailed information
+            if ( ! $check['ownership']['is_wp_owner'] ) {
+                // Handle incorrect ownership
+                write_log( 'File not owned by WordPress user, File Name: ' . $path, __METHOD__ );
+            }
+
+            if ( ! empty( $check['security']['warnings'] ) ) {
+                // Handle security warnings
+                foreach ( $check['security']['warnings'] as $warning ) {
+                    write_log( 'Security warning: ' . $warning, __METHOD__ );
+                }
+            }
+
+            return $check;
+        }
+
+        /**
+         * Check if the given path is valid further processing
+         *
+         * @package $path   Absolute path to the file or directory
+         * @return  boolean|WP_Error    true on success
+         */
+        private function is_valid_path( string $path , $enforec_ownership_check = false) {
+            global $wpss;
+            if ( ! $this->is_within_wordpress( $path ) ) {
+                wpss_logger('Info ', 'Path is not within WordPress installation, ' . $path, __METHOD__);
+                return new WP_Error(
+                    'invalid_path',
+                    __( 'Path is not within WordPress installation', $wpss->domain ),
+                    $path
+                );
+            }
+            $is_owned = $this->is_wp_owner( $path );
+            if ( is_wp_error( $is_owned ) ) {
+                wpss_logger('Info: ' , 'Path is not ownerd by WordPress' . $path, __METHOD__);
+                if($enforec_ownership_check){
+                    return new WP_Error(
+                        'ownership_failed',
+                        __( 'Path is not ownerd by WordPress', $wpss->domain ),
+                        $path
+                    );
+                }  
+            }
+            return true;
+        }//end is_valid_path()
 }
--- wpss-logger.php
+++ wpss-logger.php
@@ -5,21 +5,39 @@
  */
 if ( ! function_exists( 'write_log' ) ) {
 
-function write_log( $log, $function = __FUNCTION__ ) {
+    function write_log( $log, $function = __FUNCTION__ ) {
 
-	$formatted_log = 'WPSS-DEBUG:[' . date( 'Y-m-d H:i:s' ) . '] ' . ' Function: ' . $function . ' ';
-	if ( is_array( $log ) || is_object( $log ) ) {
-		$formatted_log .= print_r( $log, true );
-	} else {
-		$formatted_log .= $log;
-	}
+        $formatted_log = 'WPSS-DEBUG:[' . date( 'Y-m-d H:i:s' ) . '] ' . ' Function: ' . $function . ' ';
+        if ( is_array( $log ) || is_object( $log ) ) {
+            $formatted_log .= print_r( $log, true );
+        } else {
+            $formatted_log .= $log;
+        }
 
-	$formatted_log .= PHP_EOL;
+        $formatted_log .= PHP_EOL;
 
-	if ( true === WP_DEBUG && defined( WPSS_ROOT ) ) {
-		// $log_file = WP_CONTENT_DIR . '/wpss.log';
-		$log_file = WPSS_ROOT . 'wpss.log';
+        if ( true === WP_DEBUG && defined( WPSS_ROOT ) ) {
+            // $log_file = WP_CONTENT_DIR . '/wpss.log';
+            $log_file = WPSS_ROOT . 'wpss.log';
 
-		file_put_contents( $log_file, $formatted_log, FILE_APPEND );
-	}
+            file_put_contents( $log_file, $formatted_log, FILE_APPEND );
+        }
+    }
+}
+
+if ( ! function_exists( 'wpss_logger' ) ) {
+
+    function wpss_logger( $type, $log, $function ) {
+
+        $formatted_log = $type . ': [' . date( 'Y-m-d H:i:s' ) . '] ' . ' Function: ' . $function . ' ';
+        if ( is_array( $log ) || is_object( $log ) ) {
+            $formatted_log .= print_r( $log, true );
+        } else {
+            $formatted_log .= $log;
+        }
+
+        $formatted_log .= PHP_EOL;
+
+        error_log( $formatted_log );
+    }
 }
