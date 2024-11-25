2c5f4c3300ffe5133f1b983fd8123bf3f8d783a0
Deep Rahman <dp.rahman@gmail.com>
2024-11-25 20:36:27 +0600

    Fixed: Plagin activation error message

--- includes/class-wp-securing-setup.php
+++ includes/class-wp-securing-setup.php
@@ -47,7 +47,7 @@ class WP_Securing_Setup {
 	public $fpm;
 
 	public function __construct() {
-		$this->name         = __( 'WP Securing Setup', $this->domain );
+		$this->name         = __( 'WP Securing Setup', self::DOMAIN );
 		$this->root         = self::ROOT;
 		$this->domain       = self::DOMAIN;
 		$this->root_url     = WPSS_URL;
@@ -55,81 +55,91 @@ class WP_Securing_Setup {
 		$this->css_handle   = 'wpss-primary-css';
 		$this->nonce_action = 'wpss-rest';
 		$this->settings     = WPSS_SETTINGS;
-
-		// $this->file_paths = ["wp-config.php", "wp-login.php", "wp-content", "wp-content/uploads", "wp-content/plugins", "wp-content/themes"];
-		$this->file_paths  = $this->get_file_paths();
-		$this->rcmnd_perms = $this->get_rcmnd_perms();
-		$this->set_fpm();
-		$this->init();
+                $this->init();
 	}
 
 	public function init() {
-		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_admin_js' ) );
-		$this->admin_pages();
-		$this->xml_rpc_config();
-		$this->admin_rest();
-	}
+            // $this->file_paths = ["wp-config.php", "wp-login.php", "wp-content", "wp-content/uploads", "wp-content/plugins", "wp-content/themes"];
+                add_action('plugin_loaded', function(){
 
-	public function enqueue_admin_js( $admin_page ) {
-		global $wpss;
-		wp_enqueue_script( 'wp-api-request' );
-		include_once $this->root . '/includes/enqueue-scripts/wpss-enqueue-admin-scripts.php';
-	}
+                $this->file_paths  = $this->get_file_paths();
+                $this->rcmnd_perms = $this->get_rcmnd_perms();
+                $this->set_fpm();
+                });
+                add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_admin_js' ) );
+                add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_admin_css' ) );
+                $this->admin_pages();
+                $this->xml_rpc_config();
+                $this->admin_rest();
+        }
 
-	public function enqueue_admin_css() {
-	}
+        public function enqueue_admin_js( $admin_page ) {
+                global $wpss;
+                wp_enqueue_script( 'wp-api-request' );
+                include_once $this->root . '/includes/enqueue-scripts/wpss-enqueue-admin-scripts.php';
+        }
 
-	public function admin_pages() {
-		global $wpss;
-		include_once $this->root . '/admin/wpss-files-permissions-tools-page.php';
-	}
+        public function enqueue_admin_css() {
+        }
 
-	public function admin_rest() {
-		include_once $this->root . '/includes/REST/file-permission.php';
-		include_once $this->root . '/includes/REST/htaccess-protect.php';
-	}
+        public function admin_pages() {
+                global $wpss;
+                include_once $this->root . '/admin/wpss-files-permissions-tools-page.php';
+        }
 
-	public function xml_rpc_config() {
-		require_once $this->root . DIRECTORY_SEPARATOR . 'includes/wpss-xml-rpc.php';
-	}
+        public function admin_rest() {
+                include_once $this->root . '/includes/REST/file-permission.php';
+                include_once $this->root . '/includes/REST/htaccess-protect.php';
+        }
 
-	/**
-	 * Set file permission manager
-	 *
-	 * @return $this
-	 */
-	public function set_fpm() {
-		require_once $this->root . 'includes/class-wpss-file-permission-manager.php';
-		$this->fpm = new WPSS_File_Permission_Manager( $this->file_paths, $this->rcmnd_perms );
-		return $this;
-	}
-	public function get_fpm(): WPSS_File_Permission_Manager {
-		return ( ! isset( $this->fpm ) || empty( $this->fpm ) ) ? $this->set_fpm()->fpm : $this->fpm;
-	}
+        public function xml_rpc_config() {
+                require_once $this->root . DIRECTORY_SEPARATOR . 'includes/wpss-xml-rpc.php';
+        }
 
-	public function get_extension_map() {
-		return ( get_option( $this->settings ) )['htaccess']['extension_map'];
-	}
+        /**
+         * Set file permission manager
+         *
+         * @return $this
+         */
+        public function set_fpm() {
+                require_once $this->root . 'includes/class-wpss-file-permission-manager.php';
+                $this->fpm = new WPSS_File_Permission_Manager( $this->file_paths, $this->rcmnd_perms );
+                return $this;
+        }
+        public function get_fpm(): WPSS_File_Permission_Manager {
+                return ( ! isset( $this->fpm ) || empty( $this->fpm ) ) ? $this->set_fpm()->fpm : $this->fpm;
+        }
 
-	public function get_ht_form() {
-		return ( get_option( $this->settings ) )['htaccess']['ht_form'];
-	}
+        public function get_extension_map() {
+                $opt = get_option( $this->settings );    
+                return isset(( $opt )['htaccess']['extension_map']) ? ( $opt )['htaccess']['extension_map'] : null;
+        }
 
-	public function get_file_types() {
-		return ( get_option( $this->settings ) )['htaccess']['file_types'];
-	}
+        public function get_ht_form() {
 
-	public function get_original_permission() {
+                $opt = get_option( $this->settings );    
+                return isset(( $opt )['htaccess']['ht_form']) ? ( $opt )['htaccess']['ht_form'] : null;
+        }
 
-		return ( get_option( $this->settings ) )['file_permission']['chk_results'];
-	}
+        public function get_file_types() {
+                $opt = get_option( $this->settings );    
+                return isset(( $opt ) ['htaccess']['file_types']) ? ( $opt ) ['htaccess']['file_types'] : null;
+        }
 
-	public function get_file_paths() {
+        public function get_original_permission() {
+                $opt = get_option( $this->settings );    
+                return isset(( $opt ) ['file_permission']['chk_results']) ? (( $opt ) ['file_permission']['chk_results']) : null;
+        }
 
-		return ( get_option( $this->settings ) )['file_permission']['paths'];
-	}
+        public function get_file_paths() {
 
-	public function get_rcmnd_perms() {
-		return ( get_option( $this->settings ) )['file_permission']['rcmnd_perms'];
-	}
+
+                $opt = !empty(get_option( $this->settings )) ? get_option( $this->settings )  : null; 
+		return isset(( $opt  )['file_permission']['paths']) ? ( $opt )['file_permission']['paths'] : null;
+        }
+
+        public function get_rcmnd_perms() {
+                $opt = !empty(get_option( $this->settings )) ? get_option( $this->settings )  : null; 
+		return (( $opt )['file_permission']['rcmnd_perms'])? ( $opt )['file_permission']['rcmnd_perms'] : null;
+        }
 }
