0498c40b4a9ccaa8cd0ccb47fbfe508e84212aa4
Deep Rahman <dp.rahman@gmail.com>
2024-11-28 20:21:31 +0600

    Enforecd WP Coding standard

--- admin/templates/page.htm.php
+++ admin/templates/page.htm.php
@@ -1,31 +1,29 @@
 <?php
 /**
  * Main Plugin Page
- *
  */
-global $wpss;
 ?>
 
-
-<h1 id="wpss-page-heading"> <?php _e("WP Securing Setup", $wpss->domain) ?> </h1>
+<h1 id="wpss-page-heading"> <?php esc_html_e('WP Securing Setup', WP_Securing_Setup::DOMAIN); ?> </h1>
 <hr>
-<h2  id="wpss-tab-heading"> <?php _e("File Permission Page", $wpss->domain) ?> </h2>
+<h2 id="wpss-tab-heading"> <?php esc_html_e('File Permission Page', WP_Securing_Setup::DOMAIN); ?> </h2>
 
 <div id="my-tabs">
-  <ul>
-    <li><a href="#tab-1"><?php _e("File Permission Page", $wpss->domain) ?></a></li>
-    <li><a href="#tab-2"><?php _e(".htacces Config", $wpss->domain) ?></a></li>
-    <li><a href="#tab-3"><?php _e("Site Migration", $wpss->domain) ?></a></li>
-  </ul>
-  <div id="tab-1">
-    <wp-permissions-table></wp-permissions-table>
-  </div>
-  <div id="tab-2">
-    <?php include_once( $wpss->root . DIRECTORY_SEPARATOR . "admin/templates/protection-form.htm.php" );?>
-  </div>
-  <div id="tab-3">
-    <form id="form-3" class="tab-form" disabled>
-        <h3><?php _e("Comming Soon...", $wpss->domain) ?></h3>
-    </form>
-  </div>
+    <ul>
+        <li><a href="#tab-1"><?php esc_html_e('File Permission Page', WP_Securing_Setup::DOMAIN); ?></a></li>
+        <li><a href="#tab-2"><?php esc_html_e('.htacces Config', WP_Securing_Setup::DOMAIN); ?></a></li>
+        <li><a href="#tab-3"><?php esc_html_e('Site Migration', WP_Securing_Setup::DOMAIN); ?></a></li>
+    </ul>
+    <div id="tab-1">
+        <wp-permissions-table></wp-permissions-table>
+    </div>
+    <div id="tab-2">
+        <?php require_once WP_Securing_Setup::ROOT . DIRECTORY_SEPARATOR . 'admin/templates/protection-form.htm.php'; ?>
+    </div>
+    <div id="tab-3">
+        <form id="form-3" class="tab-form" disabled>
+            <h3><?php esc_html_e('Comming Soon...', WP_Securing_Setup::DOMAIN); ?></h3>
+        </form>
+    </div>
 </div>
+
--- admin/templates/protection-form.htm.php
+++ admin/templates/protection-form.htm.php
@@ -7,38 +7,46 @@ global $wpss;
 
 $file_types = $wpss->get_file_types();
 
-
 ?>
 
 <form class="htaccess-form">
     <h3 class="">Protection Settings</h3>
     <ul>
         <li>
-        <input type="checkbox" id="protect-debug-log" name="protect-debug-log" >
-            <label for="protect-debug-log" title="<?= __("Protect the WordPress log at default location")?>">Protect Debug Log</label>
+            <input type="checkbox" id="protect-debug-log" name="protect-debug-log">
+            <label for="protect-debug-log" title="<?php echo esc_attr__('Protect the WordPress log at default location', WP_Securing_Setup::DOMAIN); ?>">
+                <?php echo esc_html__('Protect Debug Log', WP_Securing_Setup::DOMAIN); ?>
+            </label>
         </li>
         <li>
             <input type="checkbox" id="protect-update-directory" name="protect-update-directory">
-            <label for="protect-update-directory" title="<?= __("Select which file-types should have access to uploads directory",WP_Securing_Setup::DOMAIN) ?>">Protect Update Directory</label>
+            <label for="protect-update-directory" title="<?php echo esc_attr__('Select which file-types should have access to uploads directory', WP_Securing_Setup::DOMAIN); ?>">
+                <?php echo esc_html__('Protect Update Directory', WP_Securing_Setup::DOMAIN); ?>
+            </label>
             <div id="update-directory-options" style="display: none;">
-            <h4><?= __("Give access to the selected file-types only", WP_Securing_Setup::DOMAIN) ?></h4>
-                <select id="mySelect" name="allowed_files"  multiple="multiple">
-                <?php foreach($file_types as $ext): ?>
-                    <option value="<?= $ext ?>"><?= strtoupper($ext) ?></option>
-                <?php endforeach; ?> 
-                    
+                <h4><?php echo esc_html__('Give access to the selected file-types only', WP_Securing_Setup::DOMAIN); ?></h4>
+                <select id="mySelect" name="allowed_files" multiple="multiple">
+                    <?php foreach ($file_types as $ext): ?>
+                        <option value="<?php echo esc_attr($ext); ?>"><?php echo esc_html(strtoupper($ext)); ?></option>
+                    <?php endforeach; ?>
                 </select>
             </div>
         </li>
         <li>
             <input type="checkbox" id="protect-xml-rpc" name="protect-xml-rpc">
-            <label for="protect-xml-rpc" title="<?= __("Disable the system.multicall method", WP_Securing_Setup::DOMAIN)?>">Protect XML-RPC</label>
+            <label for="protect-xml-rpc" title="<?php echo esc_attr__('Disable the system.multicall method', WP_Securing_Setup::DOMAIN); ?>">
+                <?php echo esc_html__('Protect XML-RPC', WP_Securing_Setup::DOMAIN); ?>
+            </label>
         </li>
         <li>
-        <input type="checkbox" id="protect-rest-endpoint" name="protect-rest-endpoint" >
-            <label for="protect-rest-endpoint" title="<?= __("Redirect requests to the users REST endpoint to 404 HTTP error",WP_Securing_Setup::DOMAIN) ?>">Protect REST Endpoint</label>
+            <input type="checkbox" id="protect-rest-endpoint" name="protect-rest-endpoint">
+            <label for="protect-rest-endpoint" title="<?php echo esc_attr__('Redirect requests to the users REST endpoint to 404 HTTP error', WP_Securing_Setup::DOMAIN); ?>">
+                <?php echo esc_html__('Protect REST Endpoint', WP_Securing_Setup::DOMAIN); ?>
+            </label>
         </li>
     </ul>
-    <button type="submit" class="button button-primary">Save Settings</button>
+    <button type="submit" class="button button-primary">
+        <?php echo esc_html__('Save Settings', WP_Securing_Setup::DOMAIN); ?>
+    </button>
 </form>
 
