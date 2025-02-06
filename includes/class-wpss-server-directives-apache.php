e9c81786c3decc04ea027b001e74f4b9e1474d80
Deep Rahman <dp.rahman@gmail.com>
2025-02-06 19:59:27 +0600

    Heredoc replaced

--- includes/class-wpss-server-directives-apache.php
+++ includes/class-wpss-server-directives-apache.php
@@ -2,62 +2,64 @@
 
 require_once WP_Securing_Setup::ROOT . DIRECTORY_SEPARATOR . 'includes/class-wpss-server-directives.php';
 require_once WP_Securing_Setup::ROOT . DIRECTORY_SEPARATOR . 'includes/interface-wpss-server-directives.php';
-require_once ABSPATH . 'wp-admin/includes/misc.php';
+
 
 class WPSS_Server_Directives_Apache extends WPSS_Server_Directives implements IWPSS_Server_Directives
 {
 
 
-    public function __construct( $cli_args = array() )
+    public function __construct($cli_args = array())
     {
         parent::__construct();
 
-        if (! empty($cli_args) ) {
+        if (!empty($cli_args)) {
             $this->is_apache = isset($cli_args['apache']) ? $cli_args['apache'] : true;
         }
     }
 
-    public function add_rule( $rules, $htaccess_path = '', $marker = 'wpss' ): bool
+    public function add_rule($rules, $htaccess_path = '', $marker = 'wpss'): bool
     {
 
-        if ($this->is_apache || $this->is_litespeed ) {
+        if ($this->is_apache || $this->is_litespeed) {
             return $this->add_apache_rule($rules, $htaccess_path, $marker);
         }
         return false;
     }
 
-    public function remove_rule( $htaccess_path = '', string $marker = 'wpss' )
+    public function remove_rule($htaccess_path = '', string $marker = 'wpss')
     {
 
-        if ($this->is_apache || $this->is_litespeed ) {
+        if ($this->is_apache || $this->is_litespeed) {
             return $this->remove_apache_rule($htaccess_path, $marker);
         }
         return false;
     }
 
-    private function add_apache_rule( $rules, $htaccess_path = '', string $marker = 'wpss' )
+    private function add_apache_rule($rules, $htaccess_path = '', string $marker = 'wpss')
     {
 
-        if (! $this->validate_htaccess_syntax($rules) ) {
+        if (!$this->validate_htaccess_syntax($rules)) {
             return false;
         }
 
         $htaccess_file = $htaccess_path ?: $this->home_path . '.htaccess';
 
-        if (! $this->wp_filesystem->exists($htaccess_file) ) {
-            if (! $this->wp_filesystem->is_writable(dirname($htaccess_file)) ) {
+        if (!$this->wp_filesystem->exists($htaccess_file)) {
+            if (!$this->wp_filesystem->is_writable(dirname($htaccess_file))) {
                 return false;
             }
             $this->wp_filesystem->touch($htaccess_file);
-        } elseif (! $this->wp_filesystem->is_writable($htaccess_file) ) {
+        } elseif (!$this->wp_filesystem->is_writable($htaccess_file)) {
             return false;
         }
-
+        if (!function_exists('extract_from_markers')) {
+            require_once ABSPATH . 'wp-admin/includes/misc.php';
+        }
         $current_rules = extract_from_markers($htaccess_file, $marker);
 
         // Read the contents of the htaccess file
         $htaccess_content = $this->wp_filesystem->get_contents($htaccess_file);
-        if ($htaccess_content === false ) {
+        if ($htaccess_content === false) {
             return false;
         }
 
@@ -65,7 +67,7 @@ class WPSS_Server_Directives_Apache extends WPSS_Server_Directives implements IW
         $lines = explode("\n", $htaccess_content);
 
         // Remove trailing blank lines
-        while ( ! empty($lines) && trim(end($lines)) === '' ) {
+        while (!empty($lines) && trim(end($lines)) === '') {
             array_pop($lines); // Remove empty lines from the end
         }
 
@@ -73,13 +75,15 @@ class WPSS_Server_Directives_Apache extends WPSS_Server_Directives implements IW
         $cleaned_htaccess_content = implode("\n", $lines);
 
         // Write the cleaned content back to the file
-        if (! $this->wp_filesystem->put_contents($htaccess_file, $cleaned_htaccess_content) ) {
+        if (!$this->wp_filesystem->put_contents($htaccess_file, $cleaned_htaccess_content)) {
             return false;
         }
 
         // Now, merge the current rules with the new rules
         $new_rules = array_merge($current_rules, explode("\n", $rules));
-
+        if (!function_exists('extract_from_markers')) {
+            require_once ABSPATH . 'wp-admin/includes/misc.php';
+        }
         // Insert the new rules with the marker
         return insert_with_markers($htaccess_file, $marker, $new_rules);
     }
@@ -91,45 +95,47 @@ class WPSS_Server_Directives_Apache extends WPSS_Server_Directives implements IW
      * @param  string $marker
      * @return bool
      */
-    private function remove_apache_rule( $htaccess_path = '', string $marker = 'wpss' )
+    private function remove_apache_rule($htaccess_path = '', string $marker = 'wpss')
     {
         $htaccess_file = $htaccess_path ?: $this->home_path . '.htaccess';
 
-        if (! $this->wp_filesystem->exists($htaccess_file) ) {
+        if (!$this->wp_filesystem->exists($htaccess_file)) {
             return false;
         }
 
-        if (! $this->wp_filesystem->is_writable($htaccess_file) ) {
+        if (!$this->wp_filesystem->is_writable($htaccess_file)) {
             return false;
         }
-
+        if (!function_exists('extract_from_markers')) {
+            require_once ABSPATH . 'wp-admin/includes/misc.php';
+        }
         // Extract the current rules for the given marker
         $extracted_rules = extract_from_markers($htaccess_file, $marker);
 
-        if (empty($extracted_rules) ) {
+        if (empty($extracted_rules)) {
             return true; // Nothing to remove, so we consider it a success
         }
 
         // Get the current content of the .htaccess file
         $current_content = $this->wp_filesystem->get_contents($htaccess_file);
-        if ($current_content === false ) {
+        if ($current_content === false) {
             return false;
         }
 
         // Remove the extracted rules from the file content
         $start_marker = "# BEGIN {$marker}";
-        $end_marker   = "# END {$marker}";
-        $pattern      = "/{$start_marker}.*?{$end_marker}\s*/s";
-        $new_content  = preg_replace($pattern, '', $current_content);
+        $end_marker = "# END {$marker}";
+        $pattern = "/{$start_marker}.*?{$end_marker}\s*/s";
+        $new_content = preg_replace($pattern, '', $current_content);
 
-        if ($new_content === null ) {
+        if ($new_content === null) {
             return false;
         }
 
         // Write the updated content back to the file
         $result = $this->wp_filesystem->put_contents($htaccess_file, $new_content);
 
-        if ($result === false ) {
+        if ($result === false) {
             return false;
         }
 
@@ -137,16 +143,17 @@ class WPSS_Server_Directives_Apache extends WPSS_Server_Directives implements IW
     }
 
     public function protect_debug_log()
-    {
-        $htaccess_path = WP_CONTENT_DIR . DIRECTORY_SEPARATOR . '.htaccess';
-        $rules         = <<<EOD
-<Files debug.log>
-    Order allow,deny
-    Deny from all
-</Files>
-EOD;
-        return $this->add_rule($rules, $htaccess_path, 'protect-log');
-    }
+{
+    $htaccess_path = WP_CONTENT_DIR . DIRECTORY_SEPARATOR . '.htaccess';
+    
+    $rules = "<Files debug.log>\n";
+    $rules .= "    Order allow,deny\n";
+    $rules .= "    Deny from all\n";
+    $rules .= "</Files>\n";
+
+    return $this->add_rule($rules, $htaccess_path, 'protect-log');
+}
+
 
     public function unprotect_debug_log()
     {
@@ -154,33 +161,22 @@ EOD;
         return $this->remove_rule($htaccess_path, 'protect-log');
     }
 
-    public function protect_user_rest_apt( $page = 'home' )
-    {
-        // NOTE: Configuration not suitable for all setup
-        $htaccess_path = ABSPATH . '.htaccess';
-        /*
-        $rules         = <<<EOD
-        RewriteEngine On
+    public function protect_user_rest_apt($page = 'home')
+{
+    // NOTE: Configuration not suitable for all setups
+    $htaccess_path = ABSPATH . '.htaccess';
 
-        # Block access to the users endpoint for any version of the API
-        RewriteRule ^wp-json/wp/v[0-9]+/users.*$ - [R=404,L]
+    $rules  = "# Custom Rate Limiting for /wp-json/wp/v2/users\n";
+    $rules .= "<IfModule mod_ratelimit.c>\n";
+    $rules .= "    <Location /wp-json/wp/v2/users>\n";
+    $rules .= "        SetOutputFilter RATE_LIMIT\n";
+    $rules .= "        SetEnv rate-limit 10\n";
+    $rules .= "    </Location>\n";
+    $rules .= "</IfModule>\n";
 
-        # Redirect query strings with author to the provided page
-        RewriteCond %{QUERY_STRING} author=\d
-        RewriteRule (.*) {$page} [L,R=301,QSD]
-        EOD;
-        */
-        $rules = <<<EOD
-# Custom Rate Limiting for /wp-json/wp/v2/users
-<IfModule mod_ratelimit.c>
-    <Location /wp-json/wp/v2/users>
-        SetOutputFilter RATE_LIMIT
-        SetEnv rate-limit 10
-    </Location>
-</IfModule>
-EOD;
-        return $this->add_rule($rules, $htaccess_path, 'protect-rest-api');
-    }
+    return $this->add_rule($rules, $htaccess_path, 'protect-rest-api');
+}
+
 
     public function unprotect_user_rest_apt()
     {
@@ -189,30 +185,32 @@ EOD;
         return $this->remove_rule($htaccess_path, 'protect-rest-api');
     }
 
-    public function allow_file_access( $file_pattern )
+    public function allow_file_access($file_pattern)
     {
         $file_pattern_regex = $this->file_ext_regex_creator($file_pattern);
-        $htaccess_path      = WP_CONTENT_DIR . '/uploads/.htaccess';
-        $rules              = <<<EOD
-<FilesMatch "{$file_pattern_regex}">
-    Require all granted
-</FilesMatch>
-EOD;
-        $res                = $this->add_rule($rules, $htaccess_path, 'protect-uploads');
-        if ($res ) {
-            $rules = <<<EOD
-<FilesMatch ".*">
-    Require all denied
-</FilesMatch>           
-EOD;
-            $res   = $this->add_rule($rules, $htaccess_path, 'protect-uploads');
-            if (! $res ) {
+        $htaccess_path = WP_CONTENT_DIR . '/uploads/.htaccess';
+    
+        $rules  = "<FilesMatch \"{$file_pattern_regex}\">\n";
+        $rules .= "    Require all granted\n";
+        $rules .= "</FilesMatch>\n";
+    
+        $res = $this->add_rule($rules, $htaccess_path, 'protect-uploads');
+    
+        if ($res) {
+            $rules  = "<FilesMatch \".*\">\n";
+            $rules .= "    Require all denied\n";
+            $rules .= "</FilesMatch>\n";
+    
+            $res = $this->add_rule($rules, $htaccess_path, 'protect-uploads');
+    
+            if (!$res) {
                 $this->remove_apache_rule($htaccess_path, 'protect-uploads');
             }
         }
+    
         return $res;
     }
-
+    
     public function disallow_file_access()
     {
         $htaccess_path = WP_CONTENT_DIR . '/uploads/.htaccess';
@@ -226,11 +224,11 @@ EOD;
      * @param  array $file_ext Contains file extension
      * @return mixed regex string of file extension
      */
-    protected function file_ext_regex_creator( array $file_ext ): mixed
+    protected function file_ext_regex_creator(array $file_ext): mixed
     {
         global $wpss;
         $file_path = $wpss->root . 'includes/class-wpss-file-regex-pattern-creator.php';
-        if (! file_exists($file_path) ) {
+        if (!file_exists($file_path)) {
             return new WP_Error('File Not Exists: ' . $file_path);
         }
         include_once $file_path;
@@ -242,11 +240,11 @@ EOD;
         $regex = $regex_pat->generateApacheExtensionRegex();
         return $regex;
     }
-    protected function validate_htaccess_syntax( string $rules ): mixed
+    protected function validate_htaccess_syntax(string $rules): mixed
     {
         global $wpss;
         $file_path = $wpss->root . 'includes/class-wpss-apache-directives-validator.php';
-        if (! file_exists($file_path) ) {
+        if (!file_exists($file_path)) {
             return new WP_Error('File Not Exists: ' . $file_path);
         }
         include_once $file_path;
