19abf08cfcfdefb0846dd2efad399de9f5927e0c
Deep <dp.rahman@gmail.com>
2025-02-13 23:26:13 +0600

    OS check function hook added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import './index.scss';
+
 
 import './wpss-files-permissions-request.js';
 import './wpss-htaccess-form-multiselect.js';
 import './wpss-htaccess-protect.js';
 
 import './wpss-htaccess-protect-from.js';
+
+import './index.scss';
 
 jQuery( document ).ready( function ( $ ) {
 	console.log( 'Script Loaded' );
