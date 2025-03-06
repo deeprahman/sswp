9cbcfd1d3baa82ce1e5cf61e34acf59dbaebbbda
deeprahman <dp.rahman@gmail.com>
2025-03-07 03:43:13 +0600

    Webpack config modified

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,28 @@
-const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
+const defaultConfig = require('@wordpress/scripts/config/webpack.config');
+const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = {
-	...defaultConfig,
-	externals: {
-		jquery: 'jQuery',
-	},
+module.exports = (env, argv) => {
+    const config = {
+        ...defaultConfig,
+        externals: {
+            jquery: 'jQuery',
+        },
+    };
+
+    if (argv.mode === 'production') {
+        config.optimization = {
+            ...config.optimization,
+            minimizer: [
+                new TerserPlugin({
+                    terserOptions: {
+                        compress: {
+                            drop_console: true,
+                        },
+                    },
+                }),
+            ],
+        };
+    }
+
+    return config;
 };
